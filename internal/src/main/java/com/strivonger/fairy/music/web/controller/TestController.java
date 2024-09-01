4409d901808b059b04afa279c8136112f642827b
Striveonger <striveonger@163.com>
2024-09-01 21:57:38 +0800

    1. id gen test

--- internal/src/main/java/com/strivonger/fairy/music/web/controller/TestController.java
+++ internal/src/main/java/com/strivonger/fairy/music/web/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.strivonger.fairy.music.web.controller;
 
+import cn.hutool.core.lang.Dict;
 import com.striveonger.common.core.result.Result;
+import com.striveonger.common.leaf.core.ID;
 import com.striveonger.common.leaf.core.IDGen;
+import com.striveonger.common.leaf.service.AllocService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
@@ -20,12 +23,19 @@ public class TestController {
     private final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @Resource
-    private IDGen idGen;
+    private IDGen fitIDGen;
+
+    @Resource
+    private AllocService allocService;
 
     @RequestMapping("/hello")
     public Result hello() {
         log.info("hello");
-        log.info("defaultIDGen: {}", idGen.next());
-        return Result.success().message("Hello World!").show();
+        allocService.registerTag("A", "测试注册Tag");
+        ID id = fitIDGen.next("A");
+        log.info("fit id by tag A: {}", id);
+        return Result.success().message("Hello World!").data(Dict.of("id", id));
     }
+
+
 }
