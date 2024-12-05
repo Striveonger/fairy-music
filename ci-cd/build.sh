05f69f1bc29566b16ac8bd15c3ef3b7567781ba6
Striveonger <striveonger@163.com>
2024-12-05 13:08:00 +0800

    ci-cd

--- ci-cd/build.sh
+++ ci-cd/build.sh
@@ -11,12 +11,15 @@ APP_WORKSHOP=$(realpath "$(dirname "$0")/..")
 
 pushd "${APP_WORKSHOP}" || exit
 
+# build service
 mvn -f internal/pom.xml clean package -DskipTests
 
+# build ui
 rm -rf website/dist website/node_modules
 pnpm -C website install
 pnpm -C website run build
 
+# build image
 docker rmi fairy-music:1.0.0
 # docker rmi fairy-music-ui:1.0.0
 
